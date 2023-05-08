c0e783859c1292b0d7475ea84f1127ace4ca4bc7
andriimarmarosovych <andrewmartsyniuk@gmail.com>
2023-05-08 22:52:21 +0300

    task1 commit

--- dom.js
+++ dom.js
@@ -1,11 +1,13 @@
-const list = document.getElementById('list');
-const first = list.firstElementChild.textContent;
-const last = list.lastElementChild.textContent;
-const second = first.nextElementSibling.textContent;
-const fourth = last.previousElementSibling.textContent;
-const third = list.children[2].textContent;
+// let list = document.getElementById("list");
 
-const text = `${first}, ${last}, ${second}, ${fourth}, ${third}`;
+// let first = list.children[0];
+// let last = list.children[list.children.length - 1];
+// let second = list.children[1];
+// let fourth = list.children[3];
+// let third = list.children[2];
 
-const modal = document.getElementById('modal');
-modal.textContent = text;
+// console.log(first.innerText);   // 1
+// console.log(last.innerText);    // 5
+// console.log(second.innerText);  // 2
+// console.log(fourth.innerText);  // 4
+// console.log(third.innerText);   // 3
